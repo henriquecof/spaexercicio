f30a20564c0cb05a12f7bca9eeb366285dc3a683
almeida.cof <almeida.cof@gmail.com>
2019-12-06 10:08:25 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Aprender React
+
         </a>
       </header>
     </div>
