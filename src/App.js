a6826a4fe1b99097ea24843e28bafc5aa83d79aa
henriquecof <almeida.cof@gmail.com>
2019-12-07 10:17:26 -0300

    configuração eslint e prettier

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,8 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Aprender React
-
+          Aprender React
         </a>
       </header>
     </div>
