8570358a8520266cd4dbfdf48ba7edefa276b267
henriquecof <almeida.cof@gmail.com>
2019-12-07 11:42:39 -0300

    exclusão itens desnecessários

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Navbar, NavItem, Row } from 'react-materialize';
+
+// import { Container } from './styles';
+
+const Header = () => {
+  <Row>
+    <Navbar className="grey darken-2">
+      <NavItem href="/">Home</NavItem>
+      <NavItem href="/">Contact</NavItem>
+    </Navbar>
+  </Row>;
+};
